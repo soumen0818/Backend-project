652f878a67c6173bf1ee9937de701cdf7827bc4c
Soumen Das <sdas721444@gmail.com>
2026-01-12 23:02:05 +0530

    Add JWT authentication to user registration

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,6 +6,7 @@ const _config = {
     port:process.env.PORT,
     databaseURL:process.env.MONGO_CONNECTION_STRING,
     env:process.env.NODE_ENV,
+    jwtsecret: process.env.JWT_SECRET,
 };
 
 export const config = Object.freeze(_config)
--- src/user/userController.ts
+++ src/user/userController.ts
@@ -2,6 +2,8 @@ import type { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import userModel from "./userModel.js";
 import bcrypt from "bcryptjs";
+import { config } from "../config/config.js";
+import jwt from "jsonwebtoken";
 
 
 const createUser = async (req:Request, res:Response, next: NextFunction) =>{
@@ -26,8 +28,17 @@ const createUser = async (req:Request, res:Response, next: NextFunction) =>{
 
     const hashPassword = await bcrypt.hash(password, 10);
 
+    const newUser = await userModel.create({
+        name,
+        email,
+        password:hashPassword,
+    });
 
-    res.json ({message: "User successfully register.."})
+    //Tokes generation
+    const token = jwt.sign({sub:newUser._id}, config.jwtsecret as string,{expiresIn: "7d"});
+
+    //Response
+    res.json ({accessToken: token});
 }
 
 
